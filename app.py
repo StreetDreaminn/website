2df2b61842994f74bbbb66dc246105894dd931e3
Franklin <franklin@hotmail.co.uk>
2024-03-18 22:15:46 +0000

    Pushing latest changes.

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/music')
+def music():
+    return render_template('music.html')
+
+@app.route('/anime')
+def anime():
+    return render_template('anime.html')
+
+@app.route('/videogames')
+def videogames():
+    return render_template('videogames.html')
+
+@app.route('/sports')
+def sports():
+    return render_template('sports.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
